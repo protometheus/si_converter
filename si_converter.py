2490e644f8ca2a40c67ad3c12a0d360e93dd1a40
Mohammad Oweis <hamada.oweis@gmail.com>
2019-10-25 14:53:16 -0700

    update: port

--- si_converter.py
+++ si_converter.py
@@ -85,9 +85,9 @@ def get_conversion_factor() -> str:
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
+	app.run(debug=True, port=33507)
 # def main():
 # 	pass
 
